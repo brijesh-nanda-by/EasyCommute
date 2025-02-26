871db3af3ac26140907a9ed2be6fe3165d82d9d8
Gautam MG <gautammg@Gautams-Air.lan>
2025-02-26 13:07:47 -0500

    Setting up springboot ecosystem along with MongoDb and Swagger

---
+++ rideservice/src/main/java/com/easycommute/RideServiceApplication.java
@@ -0,0 +1,11 @@
+package com.easycommute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RideServiceApplication.class, args);
+    }
+}
---
+++ rideservice/src/main/java/com/easycommute/repository/impl/RideCustomRepositoryImpl.java
@@ -0,0 +1,11 @@
+package com.easycommute.repository.impl;
+
+import com.easycommute.repository.RideCustomRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service("RideCustomRepositoryImpl")
+@Slf4j
+public class RideCustomRepositoryImpl implements RideCustomRepository {
+    // To implement any custom query methods
+}
